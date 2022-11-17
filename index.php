5d41382d71d415d3da876ef7e710dea5bb28ff60
itz-A53-K <118420585+itz-A53-K@users.noreply.github.com>
2022-11-17 20:20:08 +0530

    login

--- index.php
+++ index.php
@@ -7,22 +7,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Samir awesome foods</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/header_footer.css">
+    <link rel="stylesheet" href="user/css/header_footer.css">
 </head>
 
 <body>
     <?php include 'user/partial/_header.php';?>
 
     <div class="loginModule" id="loginModule">
-        <form action="partial/_loginFunctional.php" method="post" class="form" id="form">
+        <form action="user/partial/_loginFunctional.php" method="post" class="form" id="form">
             <div>
-                <label for="loginUserName">Email :</label>
+                <label for="loginUserName">User Name :</label>
                 <input type="text" id="loginUserName" name="loginUserName">
             </div>
             <div>
-                <label for="loginPassword">Email :</label>
-                <input type="text" id="loginPassword" name="loginPassword">
+                <label for="loginUserEmail">Email :</label>
+                <input type="email" id="loginUserEmail" name="loginUserEmail">
             </div>
+            <div>
+                <label for="loginPassword">Email :</label>
+                <input type="password" id="loginPassword" name="loginPassword">
+            </div>
+            <button type="submit" class ="btn">Login</button>
+        </form>
+    </div>
+    <div class="signupModule" id="signupModule">
+        <form action="user/partial/_signupFunctional.php" method="post" class="form" id="form">
+            <div>
+                <label for="userName">User Name :</label>
+                <input type="text" id="userName" name="userName">
+            </div>
+            <div>
+                <label for="userEmail">Email :</label>
+                <input type="email" id="userEmail" name="userName">
+            </div>
+            <div>
+                <label for="password">Password :</label>
+                <input type="password" id="password" name="password">
+            </div>
+            <button type="submit" class ="btn">Signup</button>
         </form>
     </div>
 </body>
--- user/partial/_loginFunctional.php
+++ user/partial/_loginFunctional.php
@@ -5,10 +5,11 @@ $method= $_SERVER['REQUEST_METHOD'];
 if ($method =='POST') {
     include '_dbConnect,php';
     $loginUserName = $_POST['loginUserName'];
+    $loginUserEmail = $_POST['loginUserEmail'];
     $loginPassword = $_POST['loginPassword'];
 
-    
-    $checkUser="";
+    // INSERT INTO `users` (`sNo`, `userName`, `email`, `password`, `varificationCode`, `time`) VALUES (NULL, 'abi', 'abi@ab', 'rtrt', 'dtrt', current_timestamp());
+    $checkUser="SELECT * FROM  `users` WHERE  email='$loginUserEmail ";
     $result= mysqli_query($conn,$$checkUser);
 
     $noOfRows= mysqli_num_rows($result);
@@ -17,7 +18,7 @@ if ($method =='POST') {
     {
         $row=mysqli_fetch_assoc($result);
 
-        if(password_verify($loginPassword,$row[''])){
+        if(password_verify($loginPassword,$row['password'])){
             session_start();
 
         }
