0bbf8ad4c240ade799bb4e611ea471e4ce011280
itz-A53-K <118420585+itz-A53-K@users.noreply.github.com>
2022-12-24 14:05:16 +0530

    l

--- user/js/logout.js
+++ user/js/logout.js
@@ -1,6 +1,6 @@
 //logout
 logoutBtn= document.querySelector('.logoutBtn');
-console.log(logoutBtn);
+// console.log(logoutBtn);
 logoutBtn.addEventListener("click", (e)=>{
         if(confirm("Do you really want to logout?")) document.location = 'http://localhost/FoodFest/user/partial/_logoutFunctional.php';
      
